0ac0d59dfdbb072bc1a90caba0a4992f2a15ee4d
Esha Pandey <nianajen520@gmail.com>
2026-03-02 15:33:10 +0000

    container

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 77898
+let accountemail = "nianajen520@gmail.com"
+var accountpassword= "12345"
+acountCity = "jaipur"
+
+// accountId = 2 //
+
+accountemail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "bengal" 
+
+console.log (accountId);
+
+console.table ([accountId, accountemail, accountpassword,accountCity])

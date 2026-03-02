ff8ec3fe096378628fd123869ee921df3928f6d7
Esha Pandey <nianajen520@gmail.com>
2026-03-02 16:27:47 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version 
+ 
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log (3 
+    + 
+    3) // code readability should be high
+
+    console.log ("Esha")
+    let name = "esha"
+    let age = 18 
+    let isLoggedIn = false
+    
+    
+
+    // number=> 2 to power 53
+
+
+    console.log (typeof undefined); // undefined
+    console.log (typeof null );// objects
